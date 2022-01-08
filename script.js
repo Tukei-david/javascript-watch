7d03eec7b49d0625a5d8df56a94cd74a968a4d78
David Tukei <dtukei831@gmail.com>
2022-01-08 21:32:44 +0300

    Design Layout

---
+++ script.js
@@ -0,0 +1,10 @@
+// setInterval(myTimer, 1000)
+
+// function myTimer() {
+//     var date = new Date()
+//     document.querySelector('.watch').innerHTML = date.toLocaleTimeString()
+//     // console.log(date.toLocaleTimeString())
+// }
+
+var date = new Date()
+console.log(date.getMinutes())
